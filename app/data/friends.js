4814740bc7bac32b79e1fa4fc8770dcb1a0b796d
Akia Canada <akiacanada@yahoo.com>
2017-07-24 01:03:23 -0500

    Fourth commit

---
+++ app/data/friends.js
@@ -0,0 +1,32 @@
+[{
+  "name":"Ava",
+  "photo":"http://www.indiewire.com/wp-content/uploads/2015/05/ava-duvernay-1.jpg",
+  "scores":[
+      1,
+      3,
+      2,
+      2,
+      3,
+      4,
+      3,
+      5,
+      5,
+      3
+    ]
+},
+{
+  "name":"Harry",
+  "photo":"https://pbs.twimg.com/profile_images/688206123841671169/feDHjri8.jpg",
+  "scores":[
+      4,
+      1,
+      1,
+      2,
+      3,
+      1,
+      2,
+      3,
+      1,
+      1
+    ]
+}]
